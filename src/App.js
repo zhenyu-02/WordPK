658c39d820bc21bd878c2714bb48b12c3e6b4d5d
Zhenyu Wang <zhenyu_wang02@qq.com>
2024-11-30 16:51:40 +0800

    修改css样式

--- src/App.js
+++ src/App.js
@@ -17,18 +17,18 @@ function App() {
                 <Routes> {/* 使用 Routes 组件 */}
                     <Route path="/" element={<Home />} /> {/* 首页组件 */}
                     <Route path="/single" element={
-                        <div style={{ position: 'relative', height: '100vh', width: '100%' }}> {/* 确保容器宽高撑满 */}
+                        <div style={{ position: 'relative', height: '100vh', width: '95%' }}> {/* 确保容器宽高撑满 */}
                             <QuizGame playerName="玩家 1" words={words} />
                             <Link to="/" className="back-button" style={{ position: 'absolute', bottom: '2rem', right: '2rem' }}>返回首页</Link> {/* 添加返回首页按钮 */}
                         </div>
                     } /> {/* 单人挑战 */}
                     <Route path="/double" element={
                         <div style={{ position: 'relative', height: '100vh', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}> {/* 确保容器宽高撑满并居中内容 */}
-                            <div style={{ display: 'flex', flexDirection: 'row', width: '95%', justifyContent: 'space-around' }}> {/* 确保双人挑战的布局为左右并均匀分配空间 */}
-                                <div style={{ margin: '0 1rem' }}> {/* 添加左右间距 */}
+                            <div style={{ display: 'flex', flexDirection: 'row', width: '98%', justifyContent: 'space-around' }}> {/* 确保双人挑战的布局为左右并均匀分配空间 */}
+                                <div style={{ margin: '0 0.1rem' }}> {/* 添加左右间距 */}
                                     <QuizGame playerName="玩家 1" words={words} />
                                 </div>
-                                <div style={{ margin: '0 1rem' }}> {/* 添加左右间距 */}
+                                <div style={{ margin: '0 0.1rem' }}> {/* 添加左右间距 */}
                                     <QuizGame playerName="玩家 2" words={words} />
                                 </div>
                             </div>
